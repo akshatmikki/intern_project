c841ca936534d6c4c8d59d0b10f6435bfc4c7082
akshatmikki <71835769+akshatmikki@users.noreply.github.com>
2023-07-31 18:37:42 +0530

    Add files via upload

---
+++ body.php
@@ -0,0 +1,159 @@
+
+<div class="main main-raised">
+        <div class="container mainn-raised" style="width:100%;">
+  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+   
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="photo/banner-1.jpg" style="width:100%;">
+        
+      </div>
+
+      <div class="item">
+        <img src="photo/banner-2.jpg" style="width:100%;">
+        
+      </div>
+    
+      <div class="item">
+        <img src="photo/banner-3.jpg" style="width:100%;">
+        
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only" >Previous</span>
+    </a>
+    <a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+     
+
+
+		<!-- SECTION -->
+		<div class="section mainn mainn-raised">
+		
+		
+			<!-- container -->
+			<div class="container">
+			
+				<!-- row -->
+				<div class="row">
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=01"><div class="shop">
+							<div class="shop-img">
+								<img src="./product_images/men-watch-1.webp" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Men<br>Collection</h3>
+								<a href="product.php?p=01" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=05"><div class="shop">
+							<div class="shop-img">
+								<img src="./product_images/women-watch-1.webp" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Women<br>Collection</h3>
+								<a href="product.php?p=05" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=09"><div class="shop">
+							<div class="shop-img">
+								<img src="./product_images/kids-watch-1.webp" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Kids<br>Collection</h3>
+								<a href="product.php?p=09" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+                            </div></a>
+					</div>
+					<!-- /shop -->
+
+					<!-- /shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=13"><div class="shop">
+							<div class="shop-img">
+								<img src="./product_images/smart-1.webp" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Smart<br>Collection</h3>
+								<a href="product.php?p=13" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+		  
+		<!-- HOT DEAL SECTION -->
+		<div id="hot-deal" class="section mainn mainn-raised">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+						<div class="hot-deal">
+							<ul class="hot-deal-countdown">
+								<li>
+									<div>
+										<h3>02</h3>
+										<span>Days</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>10</h3>
+										<span>Hours</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>34</h3>
+										<span>Mins</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>60</h3>
+										<span>Secs</span>
+									</div>
+								</li>
+							</ul>
+							<h2 class="text-uppercase">hot deal this week</h2>
+							<p>New Collection Up to 50% OFF</p>
+							<a class="primary-btn cta-btn" href="store.php">Shop now</a>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /HOT DEAL SECTION -->
+</div>
+		
